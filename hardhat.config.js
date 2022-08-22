6f3a2172916f0b170c41fa38339a4c378d9f2d66
Рашид Алхоев <240p60@Rasids-MacBook-Pro.local>
2022-08-22 16:28:29 +0300

    fix and deploy to polygon mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_ALCHEMY_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
 };
